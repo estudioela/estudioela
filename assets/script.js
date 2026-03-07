9d4890f09789c13d09e48133ba43dc0297e32482
estudioela <conversa@estudioela.com>
2026-03-07 17:01:51 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // 1. Automação de Tipografia: Vogais em Itálico (IvyPresto)
+    // 1. Automação de Vogais no IvyPresto
     const editorialTitles = document.querySelectorAll('.title-editorial');
     
     editorialTitles.forEach(title => {
         const text = title.innerHTML;
-        title.innerHTML = text.replace(/([aeiouáéíóúãõâêîôû])/gi, '<span class="vowel">$1</span>');
+        // Ignora tags HTML dentro do h2 e substitui apenas as letras
+        title.innerHTML = text.replace(/(?![^<]*>)([aeiouáéíóúãõâêîôû])/gi, '<span class="vowel">$1</span>');
     });
 
-    // 2. Scroll suave da seta
+    // 2. Click suave
     const scrollBtn = document.getElementById("scroll-down");
     if(scrollBtn) {
         scrollBtn.addEventListener("click", () => {
             const nextSection = document.getElementById("sobre-nos");
-            nextSection.scrollIntoView({ behavior: "smooth" });
+            if(nextSection) {
+                nextSection.scrollIntoView({ behavior: "smooth" });
+            }
         });
     }
 });
